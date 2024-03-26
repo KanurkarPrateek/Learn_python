0114791a4f997a05e7d2ab7c3a5286f66ba2aeb2
Kanurkar Prateek <53488723+KanurkarPrateek@users.noreply.github.com>
2024-03-26 23:52:46 +0530

    added 3 files

---
+++ CheckVowels.py
@@ -0,0 +1,6 @@
+a = input("Enter a Letter")
+
+if a in "aeiouAEIOU":
+    print("It's Vowel")
+else:
+    print("It's consonent")
---
+++ Maximum.py
@@ -0,0 +1,5 @@
+a,b = map(int,input("Enter two Numbers:\n").split())
+if a > b:
+    print(a, "is maximum")
+else:
+    print(b, "is maximum")
