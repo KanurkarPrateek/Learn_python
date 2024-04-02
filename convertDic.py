78751298b3925630b6f0a6bbf165c7f7e5005cbf
Kanurkar Prateek <53488723+KanurkarPrateek@users.noreply.github.com>
2024-04-02 10:34:23 +0530

    OOP in python

--- convertDic.py
+++ convertDic.py
@@ -5,6 +5,16 @@ def create_dictionary():
     for _ in range(num_pairs):
         key = input("Enter key: ")
         value = input("Enter value: ")
+
+        
+        if value.isdigit():
+            value = int(value)
+        elif value.replace('.', '', 1).isdigit():
+            value = float(value)
+        else:
+            # If the value is not numeric, keep it as a string
+            pass
+
         dictionary[key] = value
 
     return dictionary
